0d821c92bb402a7fccde309d466daaed0af6b1fc
loope3 <icewizardwoee@protonmail.com>
2023-07-28 04:23:01 +0100

    added user login with jwt

---
+++ src/main/java/com/store/backend/service/JWTService.java
@@ -0,0 +1,44 @@
+package com.store.backend.service;
+
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.store.backend.model.LocalUser;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+public class JWTService {
+
+  @Value("${jwt.algorithm.key}")
+  private String algorithmKey;
+
+  @Value("${jwt.issuer}")
+  private String issuer;
+
+  @Value("${jwt.expiryInSeconds}")
+  private int expiryInSeconds;
+
+  private Algorithm algorithm;
+
+  private static final String USERNAME_KEY = "USERNAME";
+
+  @PostConstruct
+  public void postConstruct() {
+    algorithm = Algorithm.HMAC256(algorithmKey);
+  }
+
+  public String generateJWT(LocalUser user) {
+    return JWT.create()
+        .withClaim(USERNAME_KEY, user.getUsername())
+        .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryInSeconds)))
+        .withIssuer(issuer)
+        .sign(algorithm);
+  }
+
+}
--- src/main/java/com/store/backend/service/UserService.java
+++ src/main/java/com/store/backend/service/UserService.java
@@ -1,7 +1,11 @@
 package com.store.backend.service;
 
+	
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.store.backend.api.model.LoginBody;
 import com.store.backend.api.model.RegistrationBody;
 import com.store.backend.exception.UserAlreadyExistsException;
 import com.store.backend.model.LocalUser;
@@ -11,9 +15,13 @@ import com.store.backend.model.dao.LocalUserDAO;
 public class UserService {
 
   private LocalUserDAO localUserDAO;
+  private EncryptionService encryptionService;
+  private JWTService jwtService;
 
-  public UserService(LocalUserDAO localUserDAO) {
+  public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
     this.localUserDAO = localUserDAO;
+    this.encryptionService = encryptionService;
+    this.jwtService = jwtService;
   }
 
   public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -26,9 +34,20 @@ public class UserService {
     user.setUsername(registrationBody.getUsername());
     user.setFirstName(registrationBody.getFirstName());
     user.setLastName(registrationBody.getLastName());
-    //TODO: Encrypt passwords!!
-    user.setPassword(registrationBody.getPassword());
+    user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
     return localUserDAO.save(user);
+  }
+
+
+  public String loginUser(LoginBody loginBody) {
+    Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+    if (opUser.isPresent()) {
+      LocalUser user = opUser.get();
+      if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+        return jwtService.generateJWT(user);
+      }
+    }
+    return null;
   }
 
 }
